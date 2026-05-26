c55b759ab0b7557b31863afbad5cff243a7984a2
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-05-25 19:14:42 -0500

    feat: Induced the error.

--- goodhello.py
+++ goodhello.py
@@ -16,12 +16,3 @@ if __name__ == "__main__":
     hello()
     bye()
 
-def test_hello_prints_expected_text(capsys):
-    hello()
-    captured = capsys.readouterr()
-    assert captured.out.strip() == "Hello, DevOps learner!"
-
-def test_bye_prints_expected_text(capsys):
-    bye()
-    captured = capsys.readouterr()
-    assert captured.out.strip() == "Goodbye!"
